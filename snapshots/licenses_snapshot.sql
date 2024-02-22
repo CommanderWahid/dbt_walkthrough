2c9908bd39a2baba2ac9d34efe1cebdd8e82454f
Wahid Atoui <atouiwahid@gmail.com>
2024-02-22 16:59:50 +0100

    Add first semantic model.

--- snapshots/licenses_snapshot.sql
+++ snapshots/licenses_snapshot.sql
@@ -5,7 +5,7 @@
           pre_hook="create schema if not exists snapshots",
           target_schema='snapshots',
           strategy='timestamp',
-          unique_key='base_license_number',
+          unique_key='license_number',
           updated_at='updated_at',
           invalidate_hard_deletes=True,
           tblproperties={
